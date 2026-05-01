e78f965d623bc4eb411dbaec93551cc5b3d5c343
jpbrandes <jpbrandes800@gmail.com>
2026-04-30 21:32:40 -0300

    Lista9

--- Treino/Lista9.c
+++ Treino/Lista9.c
@@ -1,3 +1,27 @@
-//
-// Created by jpbra on 30/04/2026.
-//
+#include <stdio.h>
+
+/*
+int main()
+
+ int  n, i = 0;
+
+   printf("Digite um numero N: ");
+   scanf("%d", &n);
+
+   for (i = 0; i < n; i+=1) {
+      printf("%d\n", i);
+   }
+}
+*/
+
+int main() {
+    int n , i;
+
+    printf("Digite um numero N: ");
+    scanf("%d", &n);
+
+    for (i = n; i > 1; i-=1) {
+        printf("%d\n", n);
+    }
+
+}
