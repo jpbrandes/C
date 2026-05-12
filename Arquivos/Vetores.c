bca7752cfc7c4608c2a9bed1b7e7685897de6414
João Pedro <jpbrandes800@gmail.com>
2026-05-12 09:39:31 -0300

    Comentários

--- Arquivos/Vetores.c
+++ Arquivos/Vetores.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(){
-  int x, y, soma, vetor[12];
+  int x, y, soma, vetor[12]; // Declaração de variáveis e do vetor contendo 12 posições, começando no zero.
   
   printf("Digite 12 números para o vetor: ");
-  for (int i = 0; i < 12; i++) {
+  for (int i = 0; i < 12; i++) { // Loop for para ler posições
       printf("Posicao [%d]: ", i);
       scanf("%d", &vetor[i]);
   }
