dedefc74e225abed6bf5b3bc7d5a69d4db409b44
João Pedro <jpbrandes800@gmail.com>
2026-05-12 09:38:31 -0300

    Vetores em C

--- Arquivos/Vetores.c
+++ Arquivos/Vetores.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
 
+int main(){
+  int x, y, soma, vetor[12];
+  
+  printf("Digite 12 números para o vetor: ");
+  for (int i = 0; i < 12; i++) {
+      printf("Posicao [%d]: ", i);
+      scanf("%d", &vetor[i]);
+  }
+  
+  printf("\nDigite o valor índice de X: ");
+  scanf("%d", &x);
+  printf("Digite o valor de Y: ");
+  scanf("%d", &y);
+  
+  soma = vetor[x] + vetor[y];
+  printf("\nA soma dos valores nas posicoes %d e %d e: %d\n", x, y, soma);
+  return 0;
+}
+
+
