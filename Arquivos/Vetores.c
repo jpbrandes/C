21aad4b0acd7118d15d2557b79bbe2d0f83b7d5d
João Pedro <jpbrandes800@gmail.com>
2026-05-12 10:02:59 -0300

    Adição de outro código de vetor, exibe os números ímpares.

--- Arquivos/Vetores.c
+++ Arquivos/Vetores.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/*
 int main(){
   int x, y, soma, vetor[12]; // Declaração de variáveis e do vetor contendo 12 posições, começando no zero.
   
@@ -18,5 +19,29 @@ int main(){
   printf("\nA soma dos valores nas posicoes %d e %d e: %d\n", x, y, soma);
   return 0;
 }
+*/
+
+/*
+#include <stdio.h>
+
+int main() {
+    int vetor[10];  // Vetor declarado
+    int i, impar = 1;
+
+
+    for(i = 0; i < 10; i++) {
+        vetor[i] = impar; 
+        impar += 2; // Ele vai realizar o calculo dos números ímpares.
+    }
+
+    printf("Os 10 primeiros impares:\n");
+    for(i = 0; i < 10; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n"); // Exibe os números ímpares atráves do cálculo.
+
+    return 0;
+}
+*/
 
 
