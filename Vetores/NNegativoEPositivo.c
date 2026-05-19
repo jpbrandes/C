9b845979760e6d5bc498f12a83ec76e2681416ec
João Pedro <jpbrandes800@gmail.com>
2026-05-19 10:28:11 -0300

    Acentuação

--- Vetores/NNegativoEPositivo.c
+++ Vetores/NNegativoEPositivo.c
@@ -12,20 +12,20 @@ int main() {
 
 
     for(i = 0; i < n; i++) {
-        printf("Informe o valor da posicao %d: ", i);
+        printf("Informe o valor da posição %d: ", i);
         scanf("%d", &v[i]);
     }
 
     for(i = 0; i < n; i++){
 
         if(v[i] < 0) {
-            printf("O numero %d e menor que zero.\n", v[i]);
+            printf("O número %d é menor que zero.\n", v[i]);
         }
         else if (v[i] > 0) {
-            printf("O numero %d e maior que zero.\n", v[i]);
+            printf("O número %d é maior que zero.\n", v[i]);
         }
         else {
-            printf("O numero %d e igual a zero.\n", v[i]);
+            printf("O número %d é igual a zero.\n", v[i]);
         }
     }
 
