ed8f0a65df3e0d13619db38074927f793d7ba524
João Pedro <jpbrandes800@gmail.com>
2026-05-26 10:04:35 -0300

    Refactor academic record management functions

--- Registros/AcademicoVUnico.c
+++ Registros/AcademicoVUnico.c
@@ -3,73 +3,63 @@
 
 #define NOTAS 10
 
-int main() {
+char  ra[NOTAS][9];
+int   nota_1[NOTAS];
+int   nota_2[NOTAS];
+int   nota_exame[NOTAS];
+float media[NOTAS];
+char  situacao[NOTAS][3];
 
-    char registro_academico[NOTAS][9];
-    char situacao[NOTAS];
+void calcular(int i) {
+    int soma = nota_1[i] + nota_2[i];
 
-    int nota_1[NOTAS];
-    int nota_2[NOTAS];
-    int nota_exame[NOTAS];
-
-    float media[NOTAS];
-
-    int i;
-
-    for(i = 0; i < NOTAS; i++) {
-
-        printf("\nAluno %d\n", i + 1);
-
-        printf("RA: ");
-        scanf("%8s", registro_academico[i]);
-
-        printf("Nota 1: ");
-        scanf("%d", &nota_1[i]);
-
-        printf("Nota 2: ");
-        scanf("%d", &nota_2[i]);
-
-        if((nota_1[i] + nota_2[i]) < 140) {
-
-            printf("Nota Exame: ");
-            scanf("%d", &nota_exame[i]);
-
-            media[i] = (nota_1[i] + nota_2[i] + nota_exame[i]) / 3.0;
-
-        } else {
-
-            nota_exame[i] = -1;
-            media[i] = (nota_1[i] + nota_2[i]) / 2.0;
-        }
-
-
-        if(media[i] >= 70)
-            situacao[i] = 'A';
-        else
-            situacao[i] = 'R';
+    if (soma >= 140) {
+        nota_exame[i] = -1;
+        media[i] = soma / 2.0f;
+    } else {
+        printf("Nota do Exame: ");
+        scanf("%d", &nota_exame[i]);
+        media[i] = (soma + nota_exame[i]) / 3.0f;
     }
 
-    printf("\n\n===== RELATORIO =====\n");
+    strcpy(situacao[i], media[i] >= 70.0f ? "AP" : "RP");
+}
 
-    for(i = 0; i < NOTAS; i++) {
+void cadastrar(int *qtd) {
+    if (*qtd >= NOTAS) { printf("Turma cheia!\n"); return; }
 
-        printf("\nRA: %s", registro_academico[i]);
+    int i = *qtd;
 
-        printf("\nN1: %d", nota_1[i]);
-        printf("\nN2: %d", nota_2[i]);
+    printf("RA: ");     scanf("%8s", ra[i]);
+    printf("Nota 1: "); scanf("%d", &nota_1[i]);
+    printf("Nota 2: "); scanf("%d", &nota_2[i]);
 
-        if(nota_exame[i] != -1)
-            printf("\nExame: %d", nota_exame[i]);
-        else
-            printf("\nExame: NAO REALIZADO");
+    calcular(i);
 
-        printf("\nMedia: %.2f", media[i]);
+    printf("Media: %.1f | %s\n", media[i], situacao[i]);
+    (*qtd)++;
+}
 
-        if(situacao[i] == 'A')
-            printf("\nSituacao: APROVADO\n");
-        else
-            printf("\nSituacao: REPROVADO\n");
+void listar(int qtd) {
+    printf("\n%-10s %4s %4s %4s %7s  %s\n", "RA", "N1", "N2", "NE", "Media", "Sit.");
+    for (int i = 0; i < qtd; i++) {
+        char ne[5];
+        if (nota_exame[i] == -1) strcpy(ne, " -- ");
+        else sprintf(ne, "%4d", nota_exame[i]);
+        printf("%-10s %4d %4d %s %7.1f  %s\n",
+            ra[i], nota_1[i], nota_2[i], ne, media[i], situacao[i]);
     }
+}
+
+int main(void) {
+    int qtd = 0, op;
+
+    do {
+        printf("\n1-Cadastrar  2-Listar  0-Sair\nOpcao: ");
+        scanf("%d", &op);
+        if (op == 1) cadastrar(&qtd);
+        if (op == 2) listar(qtd);
+    } while (op != 0);
 
     return 0;
 }
