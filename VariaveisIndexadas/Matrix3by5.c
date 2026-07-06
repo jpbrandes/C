c879374529ed6aa83250b15be9c50e0ea3bdea9d
João Pedro <jpdevprojects879@gmail.com>
2026-07-06 11:38:51 -0300

    Translate comments and print statements to English

--- VariaveisIndexadas/Matrix3by5.c
+++ VariaveisIndexadas/Matrix3by5.c
@@ -1,40 +1,34 @@
 #include <stdio.h>
-
 int main() {
     float A[3][5], B[3][5], S[3][5];
     int i, j;
-
-    // Leitura da matriz A
-    printf("Digite os elementos da matriz A (3x5):\n");
+    // Reading matrix A
+    printf("Enter the elements of matrix A (3x5):\n");
     for(i = 0; i < 3; i++) {
         for(j = 0; j < 5; j++) {
             scanf("%f", &A[i][j]);
         }
     }
-
-    // Leitura da matriz B
-    printf("\nDigite os elementos da matriz B (3x5):\n");
+    // Reading matrix B
+    printf("\nEnter the elements of matrix B (3x5):\n");
     for(i = 0; i < 3; i++) {
         for(j = 0; j < 5; j++) {
             scanf("%f", &B[i][j]);
         }
     }
-
-    // Soma das matrizes
+    // Sum of the matrices
     for(i = 0; i < 3; i++) {
         for(j = 0; j < 5; j++) {
             S[i][j] = A[i][j] + B[i][j];
         }
     }
-
-    // Impressão da matriz soma
-    printf("\nMatriz Soma (A + B):\n");
+    // Printing the sum matrix
+    printf("\nSum Matrix (A + B):\n");
     for(i = 0; i < 3; i++) {
         for(j = 0; j < 5; j++) {
             printf("%.2f ", S[i][j]);
         }
         printf("\n");
     }
-
     return 0;
 }
