b8396c39036a8f978cd000b7abd01a42edb67bc6
João Pedro <jpdevprojects879@gmail.com>
2026-07-06 11:43:38 -0300

    Update and rename ConjuntosAeB.c to AandBSets.c

--- VariaveisIndexadas/AandBSets.c
+++ VariaveisIndexadas/AandBSets.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
-
 int main() {
     int A[50], B[50];
     float C[50];
     int i;
-
-    // Leitura do conjunto A
-    printf("Digite os 50 numeros do conjunto A:\n");
+    // Reading set A
+    printf("Enter the 50 numbers for set A:\n");
     for(i = 0; i < 50; i++) {
         scanf("%d", &A[i]);
     }
-
-    // Leitura do conjunto B
-    printf("Digite os 50 numeros do conjunto B:\n");
+    // Reading set B
+    printf("Enter the 50 numbers for set B:\n");
     for(i = 0; i < 50; i++) {
         scanf("%d", &B[i]);
     }
-
-    // Calcula a média entre os elementos de A e B
+    // Calculates the average between the elements of A and B
     for(i = 0; i < 50; i++) {
         C[i] = (A[i] + B[i]) / 2.0;
     }
-
-    // Exibe o conjunto C
-    printf("\nConjunto C (medias):\n");
+    // Displays set C
+    printf("\nSet C (averages):\n");
     for(i = 0; i < 50; i++) {
         printf("C[%d] = %.2f\n", i, C[i]);
     }
-
-    return 0; // O segredo é o for.
+    return 0; // The secret is the for loop.
 }
