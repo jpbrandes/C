b6e3609c7d39c730faa64d38eb67245e7f8ca443
João Pedro <jpdevprojects879@gmail.com>
2026-07-06 11:42:56 -0300

    Update comments for clarity and language change

--- VariaveisIndexadas/ChangeValues.c
+++ VariaveisIndexadas/ChangeValues.c
@@ -1,39 +1,32 @@
 #include <stdio.h>
-
 int main() {
     int A[20], B[20];
     int i, temp;
-
-    // Leitura do conjunto A
-    printf("Digite os 20 numeros do conjunto A:\n");
+    // Reading set A
+    printf("Enter the 20 numbers for set A:\n");
     for(i = 0; i < 20; i++) {
         scanf("%d", &A[i]);
     }
-
-    // Leitura do conjunto B
-    printf("Digite os 20 numeros do conjunto B:\n");
+    // Reading set B
+    printf("Enter the 20 numbers for set B:\n");
     for(i = 0; i < 20; i++) {
         scanf("%d", &B[i]);
     }
-
-    // Troca dos valores entre A e B
+    // Swapping values between A and B
     for(i = 0; i < 20; i++) {
         temp = A[i];
         A[i] = B[i];
         B[i] = temp;
     }
-
-    // Exibe o novo conjunto A
-    printf("\nNovo conjunto A:\n");
+    // Displays the new set A
+    printf("\nNew set A:\n");
     for(i = 0; i < 20; i++) {
         printf("A[%d] = %d\n", i, A[i]);
     }
-
-    // Exibe o novo conjunto B
-    printf("\nNovo conjunto B:\n");
+    // Displays the new set B
+    printf("\nNew set B:\n");
     for(i = 0; i < 20; i++) {
         printf("B[%d] = %d\n", i, B[i]);
     }
-
     return 0;
 }
