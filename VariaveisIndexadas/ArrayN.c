bec45a64c6f67ad46b33e1704f558e7a3eb455eb
João Pedro <jpdevprojects879@gmail.com>
2026-07-06 11:38:13 -0300

    Add ArrayN.c file

--- VariaveisIndexadas/ArrayN.c
+++ VariaveisIndexadas/ArrayN.c
@@ -1,42 +1,31 @@
 #include <stdio.h>
-
 int main() {
     int N, i, j;
-
-    printf("Digite o tamanho do vetor: ");
+    printf("Enter the size of the array: ");
     scanf("%d", &N);
-
-    int v[N], usado[N];
-
-    // Inicializa vetor de controle
+    int v[N], used[N];
+    // Initialize control array
     for(i = 0; i < N; i++) {
-        usado[i] = 0;
+        used[i] = 0;
     }
-
-    // Leitura do vetor
-    printf("Digite os elementos do vetor:\n");
+    // Reading the array
+    printf("Enter the elements of the array:\n");
     for(i = 0; i < N; i++) {
         scanf("%d", &v[i]);
     }
-
-    // Frequência sem repetição
-    printf("\nNumero - Frequencia\n");
-
+    // Frequency without repetition
+    printf("\nNumber - Frequency\n");
     for(i = 0; i < N; i++) {
-
-        if(usado[i] == 1)
+        if(used[i] == 1)
             continue;
-
         int freq = 1;
         for(j = i + 1; j < N; j++) {
             if(v[i] == v[j]) {
                 freq++;
-                usado[j] = 1; // marca como já contado
+                used[j] = 1; // mark as already counted
             }
         }
-
         printf("%d - %d\n", v[i], freq);
     }
-
     return 0;
 }
