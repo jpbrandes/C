377a2fc91eb46ba063978e5cf45aa2957a2c82d0
João Pedro <jpdevprojects879@gmail.com>
2026-07-06 11:41:39 -0300

    Improve user prompts and comments in NewSet.c
    Updated comments and prompts for better clarity.

--- VariaveisIndexadas/NewSet.c
+++ VariaveisIndexadas/NewSet.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
-
 int main() {
     int A[15], B[15], C[30];
     int i;
-
-    // Leitura do conjunto A
-    printf("Digite os 15 numeros do conjunto A:\n");
+    // Reading set A
+    printf("Enter the 15 numbers for set A:\n");
     for(i = 0; i < 15; i++) {
         scanf("%d", &A[i]);
     }
-
-    // Leitura do conjunto B
-    printf("Digite os 15 numeros do conjunto B:\n");
+    // Reading set B
+    printf("Enter the 15 numbers for set B:\n");
     for(i = 0; i < 15; i++) {
         scanf("%d", &B[i]);
     }
-
-    // Copia os elementos de A para C
+    // Copies the elements of A to C
     for(i = 0; i < 15; i++) {
         C[i] = A[i];
     }
-
-    // Copia os elementos de B para C
+    // Copies the elements of B to C
     for(i = 0; i < 15; i++) {
         C[i + 15] = B[i];
     }
-
-    // Exibe o conjunto C
-    printf("\nConjunto C:\n");
+    // Displays set C
+    printf("\nSet C:\n");
     for(i = 0; i < 30; i++) {
         printf("C[%d] = %d\n", i, C[i]);
     }
-
     return 0;
 }
