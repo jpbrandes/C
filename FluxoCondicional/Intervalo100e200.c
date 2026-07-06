8f9ec4734b80debefebfcb438124858294e32c17
João Pedro <jpdevprojects879@gmail.com>
2026-07-06 11:10:51 -0300

    Refactor variable names and update print messages

--- FluxoCondicional/Intervalo100e200.c
+++ FluxoCondicional/Intervalo100e200.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int numero;
+    int num;
 
-    printf("Digite um numero inteiro: ");
-    scanf("%d", &numero);
+    printf("Type a int number: ");
+    scanf("%d", &num);
     
-    if (numero >= 100 && numero <= 200) {
-        printf("O número %d está entre 100 e 200.\n", numero);
+    if (num >= 100 && num <= 200) {
+        printf("Number %d is between 100 and 200.\n", num);
     } else {
-        printf("O número %d não está entre 100 e 200.\n", numero);
+        printf("Numbee %d is not between 100 and 200.\n", num);
     }
     return 0;
 }
