8e8392b691fa56cdd4ea9fa943cb6f017f835319
João Pedro <jpdevprojects879@gmail.com>
2026-07-06 11:32:50 -0300

    Add new file 10Arrays.c

--- Vetores/10Arrays.c
+++ Vetores/10Arrays.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <locale.h>
-
 int main() {
-    setlocale(LC_ALL,"Portuguese");
+    setlocale(LC_ALL,"English");
     int i, v[10];
-
     for(i = 0; i <=9; i++) {
-        printf("Informe o valor da posição %d: ", i);
+        printf("Enter the value for position %d: ", i);
         scanf("%d", &v[i]);
-
     }
     for(i = 0; i<=9; i++){
         printf("v[%d] = %d\n", i, v[i]);
