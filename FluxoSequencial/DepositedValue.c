e851f7ab6362d7ef7064877a492381dd3348524b
João Pedro <jpdevprojects879@gmail.com>
2026-07-06 12:21:02 -0300

    Delete FluxoSequencial directory

--- FluxoSequencial/DepositedValue.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    double depositedValue, finalValue, interestRate = 0.70;
-    printf("Enter the deposited value: ");
-    scanf("%lf", &depositedValue);
-    finalValue = depositedValue + (depositedValue * (interestRate / 100));
-    printf("The final value after the deposit is: %.2lf\n", finalValue);
-    return 0;
-}
