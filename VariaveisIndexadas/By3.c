790a7cb619204afbe0ceb3c6f9ebc1fb1fde3772
João Pedro <jpdevprojects879@gmail.com>
2026-07-06 11:41:02 -0300

    Translate comments and prompts to English

--- VariaveisIndexadas/By3.c
+++ VariaveisIndexadas/By3.c
@@ -1,25 +1,20 @@
 #include <stdio.h>
-
 int main() {
     int A[30], B[30];
     int i;
-
-    // Leitura do conjunto A
-    printf("Digite os 30 numeros do conjunto A:\n");
+    // Reading set A
+    printf("Enter the 30 numbers for set A:\n");
     for(i = 0; i < 30; i++) {
         scanf("%d", &A[i]);
     }
-
-    // Multiplica os elementos de A por 3 e armazena em B
+    // Multiplies the elements of A by 3 and stores them in B
     for(i = 0; i < 30; i++) {
         B[i] = A[i] * 3;
     }
-
-    // Exibe o conjunto B
-    printf("\nConjunto B:\n");
+    // Displays set B
+    printf("\nSet B:\n");
     for(i = 0; i < 30; i++) {
         printf("B[%d] = %d\n", i, B[i]);
     }
-
     return 0;
 }
